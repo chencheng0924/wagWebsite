27f3089dc3b10d7a859d9060b7727f5285056148
chencheng0924 <abs255667@gmail.com>
2024-01-07 23:09:33 +0800

    fix/20240107 修正baseurl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/wagWebsite/',
+    // baseURL: '/wagWebsite/',
     buildAssetsDir: '/assets/'
   },
   devtools: { enabled: true },
